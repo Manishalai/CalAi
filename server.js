b41cb4a2fc4f1ee36151010774fe19da78f1729d
Manishalai <kristin.p@calai.org>
2024-05-21 15:34:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -77,8 +77,8 @@ app.post("/create-order", async (req, res) => {
     application_context: {
       brand_name: "CalAI",
       locale: "en-US",
-      return_url: "https://calai-site.vercel.app/capture/success.html", // This is the returnUrl
-      cancel_url: "https://calai-site.vercel.app/capture/cancel.html", // Your cancel URL
+      return_url: "https://calai.org/capture/success.html", // This is the returnUrl
+      cancel_url: "https://calai.org/capture/cancel.html", // Your cancel URL
     },
   };
   const accessToken = await generateToken();
